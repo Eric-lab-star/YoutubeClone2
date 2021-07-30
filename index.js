6a6325489a72173f7d8a62115540151c86ed83b1
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-07-30 23:10:05 +0900

    building server

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from "express";
 
-console.log("hello");
-console.log("hi");``
+const PORT = 4000;
+const app = express();
+
+const middleWare = (req, res, next)=> {
+ console.log(`someone is requesting ${req.url}`);
+ next();
+}
+
+const handleHome = (req, res) => {
+    return res.send("this is Home");
+}
+
+app.use( middleWare)
+app.get("/", handleHome);
+
+
+const handlePort = () => {
+    console.log(`server is listening to http://localhost:${PORT}🥶`);
+}
+app.listen(PORT, handlePort);
