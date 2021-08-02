5337deaaa74f3070726d989211637f73f9aa63f8
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-02 20:02:28 +0900

    new mixins

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,30 +1,57 @@
 const fakeUser = {
-    name: "Eric",
-    login: true,
+  name: "Eric",
+  login: false,
 };
 
-const videos = [];
+const videos = [
+  {
+    title: "video 1",
+    rating: 5,
+    comments: 2,
+    view: 12,
+  },
+  {
+    title: "video 2",
+    rating: 5,
+    comments: 2,
+    view: 12,
+  },
+  {
+    title: "video 3",
+    rating: 5,
+    comments: 2,
+    view: 12,
+  },
+
+  {
+    title: "video 4",
+    rating: 5,
+    comments: 2,
+    view: 12,
+  },
+];
 
 export const handleProfile = (req, res) => {
-    return  res.send("user profile");
-};
-  
-export const handleUser = (req, res) => res.render("user")
-  
-export const handleGlobalrouter = (req, res) => res.render("home", {title: "Home", fakeUser, videos});
-
-export const  joinController = (req, res) => {
-    return   res.send("join");
+  return res.send("user profile");
 };
 
-export const  loginController = (req, res) => {
-    return   res.send("login");
+export const handleUser = (req, res) => res.render("user");
+
+export const handleGlobalrouter = (req, res) =>
+  res.render("home", { title: "Home", fakeUser, videos });
+
+export const joinController = (req, res) => {
+  return res.send("join");
 };
 
-export const  logoutController = (req, res) => {
-    return   res.send("logout");
+export const loginController = (req, res) => {
+  return res.send("login");
 };
 
-export const  editProfile = (req, res) => {
-    return   res.send("edit your profile");
+export const logoutController = (req, res) => {
+  return res.send("logout");
+};
+
+export const editProfile = (req, res) => {
+  return res.send("edit your profile");
 };
