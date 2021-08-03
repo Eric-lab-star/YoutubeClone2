fe398671b4dd5d8dd3f239fe32b1524245a6528d
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-03 23:42:22 +0900

    my mixins

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,10 +1,12 @@
 export const handleProfile = (req, res) => res.render("userProfile");
 
-export const handleUser = (req, res) => res.render("user");
-
+export const handleUser = (req, res) =>
+  res.render("user", { pageTitle: "User" });
 export const joinController = (req, res) => res.render("join");
-export const loginController = (req, res) => res.render("login");
+export const loginController = (req, res) =>
+  res.render("login", { pageTitle: "login" });
 
-export const logoutController = (req, res) => res.render("logout");
+export const logoutController = (req, res) =>
+  res.render("logout", { pageTitle: "logout" });
 
 export const editProfile = (req, res) => res.render("editProfile");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,44 @@
-export const handleGlobalrouter = (req, res) => res.render("home");
+const videos = [
+  {
+    title: "First Video",
+    rating: 3,
+    comments: 2,
+    createdAt: "2 min ago",
+    id: 1,
+  },
+  {
+    title: "Scond Video",
+    rating: 4,
+    rating: 3,
+    comments: 2,
+    createdAt: "2 min ago",
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    rating: 3,
+    comments: 2,
+    createdAt: "2 min ago",
+    id: 3,
+  },
+  {
+    title: "Fourth Video",
+    rating: 1,
+    rating: 3,
+    comments: 2,
+    createdAt: "2 min ago",
+    id: 4,
+  },
+];
+export const handleGlobalrouter = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
 
-export const handleVideo = (req, res) => res.render("video");
+export const handleVideo = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("video", { pageTitle: video.title });
+};
 
 export const editVideo = (req, res) => res.render("editVideo");
 
