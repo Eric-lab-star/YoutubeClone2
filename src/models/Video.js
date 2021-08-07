91fbc721b55393d90875d7e8ef8244845ffcee3b
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-07 21:34:49 +0900

    mongoose installed

---
+++ src/models/Video.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+//아래에서 비디오 shape 을 정해주었다.
+const videoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  createdAt: Date,
+  hashtags: [{ type: String }],
+  meta: {
+    views: Number,
+    rating: Number,
+  },
+});
+
+const Video = mongoose.model("Video", videoSchema);
+export default Video;
